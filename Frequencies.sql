28f3d8afe34996e746930ef25a1dd6fe6cf6278f
Jack Wieland <108068654+jackwieland@users.noreply.github.com>
2024-03-23 17:53:16 +0000

    Add files via upload

---
+++ Frequencies.sql
@@ -0,0 +1,95 @@
+SELECT Start_Position, Count(Start_Position) AS Frequency
+FROM View_Name_Chromosome_1
+GROUP BY Start_Position;
+
+SELECT Start_Position, Count(Start_Position) AS Frequency
+FROM View_Name_Chromosome_2
+GROUP BY Start_Position;
+
+SELECT Start_Position, Count(Start_Position) AS Frequency
+FROM View_Name_Chromosome_3
+GROUP BY Start_Position;
+
+SELECT Start_Position, Count(Start_Position) AS Frequency
+FROM View_Name_Chromosome_4
+GROUP BY Start_Position;
+
+SELECT Start_Position, Count(Start_Position) AS Frequency
+FROM View_Name_Chromosome_5
+GROUP BY Start_Position;
+
+SELECT Start_Position, Count(Start_Position) AS Frequency
+FROM View_Name_Chromosome_6
+GROUP BY Start_Position;
+
+SELECT Start_Position, Count(Start_Position) AS Frequency
+FROM View_Name_Chromosome_7
+GROUP BY Start_Position;
+
+SELECT Start_Position, Count(Start_Position) AS Frequency
+FROM View_Name_Chromosome_8
+GROUP BY Start_Position;
+
+SELECT Start_Position, Count(Start_Position) AS Frequency
+FROM View_Name_Chromosome_9
+GROUP BY Start_Position;
+
+SELECT Start_Position, Count(Start_Position) AS Frequency
+FROM View_Name_Chromosome_10
+GROUP BY Start_Position;
+
+SELECT Start_Position, Count(Start_Position) AS Frequency
+FROM View_Name_Chromosome_11
+GROUP BY Start_Position;
+
+SELECT Start_Position, Count(Start_Position) AS Frequency
+FROM View_Name_Chromosome_12
+GROUP BY Start_Position;
+
+SELECT Start_Position, Count(Start_Position) AS Frequency
+FROM View_Name_Chromosome_13
+GROUP BY Start_Position;
+
+SELECT Start_Position, Count(Start_Position) AS Frequency
+FROM View_Name_Chromosome_14
+GROUP BY Start_Position;
+
+SELECT Start_Position, Count(Start_Position) AS Frequency
+FROM View_Name_Chromosome_15
+GROUP BY Start_Position;
+
+SELECT Start_Position, Count(Start_Position) AS Frequency
+FROM View_Name_Chromosome_16
+GROUP BY Start_Position;
+
+SELECT Start_Position, Count(Start_Position) AS Frequency
+FROM View_Name_Chromosome_17
+GROUP BY Start_Position;
+
+SELECT Start_Position, Count(Start_Position) AS Frequency
+FROM View_Name_Chromosome_18
+GROUP BY Start_Position;
+
+SELECT Start_Position, Count(Start_Position) AS Frequency
+FROM View_Name_Chromosome_19
+GROUP BY Start_Position;
+
+SELECT Start_Position, Count(Start_Position) AS Frequency
+FROM View_Name_Chromosome_20
+GROUP BY Start_Position;
+
+SELECT Start_Position, Count(Start_Position) AS Frequency
+FROM View_Name_Chromosome_21
+GROUP BY Start_Position;
+
+SELECT Start_Position, Count(Start_Position) AS Frequency
+FROM View_Name_Chromosome_22
+GROUP BY Start_Position;
+
+SELECT Start_Position, Count(Start_Position) AS Frequency
+FROM View_Name_Chromosome_X
+GROUP BY Start_Position;
+
+SELECT Start_Position, Count(Start_Position) AS Frequency
+FROM View_Name_Chromosome_Y
+GROUP BY Start_Position;
