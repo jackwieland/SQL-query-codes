39ba207565c92ed77b84f6e144e1b10507532f09
Jack Wieland <108068654+jackwieland@users.noreply.github.com>
2025-10-04 14:24:36 +0100

    Update Frequencies.sql

--- Frequencies.sql
+++ Frequencies.sql
@@ -1,95 +1,119 @@
 SELECT Start_Position, Count(Start_Position) AS Frequency
 FROM View_Name_Chromosome_1
+WHERE Start_Position >= 1 AND Start_Position <10
 GROUP BY Start_Position;
 
 SELECT Start_Position, Count(Start_Position) AS Frequency
 FROM View_Name_Chromosome_2
+WHERE Start_Position >= 1 AND Start_Position <10  
 GROUP BY Start_Position;
 
 SELECT Start_Position, Count(Start_Position) AS Frequency
 FROM View_Name_Chromosome_3
+WHERE Start_Position >= 1 AND Start_Position <10
 GROUP BY Start_Position;
 
 SELECT Start_Position, Count(Start_Position) AS Frequency
 FROM View_Name_Chromosome_4
+WHERE Start_Position >= 1 AND Start_Position <10
 GROUP BY Start_Position;
 
 SELECT Start_Position, Count(Start_Position) AS Frequency
 FROM View_Name_Chromosome_5
+WHERE Start_Position >= 1 AND Start_Position <10
 GROUP BY Start_Position;
 
 SELECT Start_Position, Count(Start_Position) AS Frequency
 FROM View_Name_Chromosome_6
+WHERE Start_Position >= 1 AND Start_Position <10
 GROUP BY Start_Position;
 
 SELECT Start_Position, Count(Start_Position) AS Frequency
 FROM View_Name_Chromosome_7
+WHERE Start_Position >= 1 AND Start_Position <10
 GROUP BY Start_Position;
 
 SELECT Start_Position, Count(Start_Position) AS Frequency
 FROM View_Name_Chromosome_8
+WHERE Start_Position >= 1 AND Start_Position <10
 GROUP BY Start_Position;
 
 SELECT Start_Position, Count(Start_Position) AS Frequency
 FROM View_Name_Chromosome_9
+WHERE Start_Position >= 1 AND Start_Position <10
 GROUP BY Start_Position;
 
 SELECT Start_Position, Count(Start_Position) AS Frequency
 FROM View_Name_Chromosome_10
+WHERE Start_Position >= 1 AND Start_Position <10
 GROUP BY Start_Position;
 
 SELECT Start_Position, Count(Start_Position) AS Frequency
 FROM View_Name_Chromosome_11
+WHERE Start_Position >= 1 AND Start_Position <10
 GROUP BY Start_Position;
 
 SELECT Start_Position, Count(Start_Position) AS Frequency
 FROM View_Name_Chromosome_12
+WHERE Start_Position >= 1 AND Start_Position <10
 GROUP BY Start_Position;
 
 SELECT Start_Position, Count(Start_Position) AS Frequency
 FROM View_Name_Chromosome_13
+WHERE Start_Position >= 1 AND Start_Position <10
 GROUP BY Start_Position;
 
 SELECT Start_Position, Count(Start_Position) AS Frequency
 FROM View_Name_Chromosome_14
+WHERE Start_Position >= 1 AND Start_Position <10
 GROUP BY Start_Position;
 
 SELECT Start_Position, Count(Start_Position) AS Frequency
 FROM View_Name_Chromosome_15
+WHERE Start_Position >= 1 AND Start_Position <10
 GROUP BY Start_Position;
 
 SELECT Start_Position, Count(Start_Position) AS Frequency
 FROM View_Name_Chromosome_16
+WHERE Start_Position >= 1 AND Start_Position <10
 GROUP BY Start_Position;
 
 SELECT Start_Position, Count(Start_Position) AS Frequency
 FROM View_Name_Chromosome_17
+WHERE Start_Position >= 1 AND Start_Position <10
 GROUP BY Start_Position;
 
 SELECT Start_Position, Count(Start_Position) AS Frequency
 FROM View_Name_Chromosome_18
+WHERE Start_Position >= 1 AND Start_Position <10
 GROUP BY Start_Position;
 
 SELECT Start_Position, Count(Start_Position) AS Frequency
 FROM View_Name_Chromosome_19
+WHERE Start_Position >= 1 AND Start_Position <10
 GROUP BY Start_Position;
 
 SELECT Start_Position, Count(Start_Position) AS Frequency
 FROM View_Name_Chromosome_20
+WHERE Start_Position >= 1 AND Start_Position <10
 GROUP BY Start_Position;
 
 SELECT Start_Position, Count(Start_Position) AS Frequency
 FROM View_Name_Chromosome_21
+WHERE Start_Position >= 1 AND Start_Position <10
 GROUP BY Start_Position;
 
 SELECT Start_Position, Count(Start_Position) AS Frequency
 FROM View_Name_Chromosome_22
+WHERE Start_Position >= 1 AND Start_Position <10
 GROUP BY Start_Position;
 
 SELECT Start_Position, Count(Start_Position) AS Frequency
 FROM View_Name_Chromosome_X
+WHERE Start_Position >= 1 AND Start_Position <10
 GROUP BY Start_Position;
 
 SELECT Start_Position, Count(Start_Position) AS Frequency
 FROM View_Name_Chromosome_Y
+WHERE Start_Position >= 1 AND Start_Position <10
 GROUP BY Start_Position;
